31201dec64e78096fd89ee8f738f85ea286578b8
Torkhwan Tussanapoom <torkhwan2282549@gmail.com>
2026-01-14 08:30:44 +0700

    Update feature-1 Calculate the time

--- car_parking_fee.py
+++ car_parking_fee.py
@@ -9,3 +9,9 @@ if(hour_out < hour_in or hour_in < 0 or hour_out < 0 or minute_in < 0 or minute_
 if(hour_out < 7 or hour_out > 23 or (hour_out == 23 and minute_out > 0) or hour_in < 7 or hour_in > 23 or (hour_in == 23 and minute_in > 0)):
     print("Invalid Input")
     exit()
+
+time_in = hour_in * 60 + minute_in
+time_out = hour_out * 60 + minute_out
+total_time = time_out - time_in
+total_hours = total_time // 60
+total_minutes = total_time % 60
